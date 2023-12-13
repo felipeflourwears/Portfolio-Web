8f36298d6a55479fc10a8c1f2081fcdab3a6f91c
felipeflourwears <A01274880@tec.mx>
2023-12-13 12:32:36 -0600

    Update About Section

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Commands to init project 39:39
+## Commands to init project 54:11
 
 ```bash
     npx create-next-app@latest
--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,50 @@ import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton';
 
+
+const TAB_DATA = [
+   {
+    title: "Skills",
+    id: "skills",
+    content: (
+        <ul className='list-disc pl-2'>
+            <li>Javascript</li>
+            <li>Learning React</li>
+            <li>Learning Next JS</li>
+            <li>Python</li>
+            <li>AWS</li>
+            <li>ROS</li>
+            <li>GIT</li>
+
+        </ul>
+    )
+   },
+   {
+    title: "Education",
+    id: "education",
+    content: (
+        <ul className='list-disc pl-2'>
+            <li>B.S. Robotics & Digital Systems Engineering</li>
+            <li>Tecnológico de Monterrey, Santa Fe Campus</li>
+        </ul>
+    )
+   },
+   {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+        <ul className='list-disc pl-2'>
+            <li>AWS Academy Cloud Foundations</li>
+            <li>Cibersecurity Training OEA</li>
+            <li>Try Hackme Practitioner</li>
+            <li>Capture the Flag OEA Cibersecurity</li>
+            <li>Python Advanced</li>
+            <li>Visual Display Solution Partner Program</li>
+        </ul>
+    )
+   }
+]
+
 const AboutSection = () => {
     const [tab, setTab] = useState("skills");
     const [isPending, startTransition] = useTransition();
@@ -16,7 +60,7 @@ const AboutSection = () => {
     <section className='text-white'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src="/images/about.png" width={500} height={500} />
-            <div>
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                 <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                 <p className='text-base lg:text-lg'>
                     I am an Engineer in Robotics and Digital Systems, a Cybersecurity Tester, and a 
@@ -26,7 +70,7 @@ const AboutSection = () => {
                     Currently, I am learning React and Next.js. I learn quickly and adapt to needs; 
                     I'm always excited to work in a team to create incredible projects. 
                 </p>
-                <div className='flex flex-row mt-8'>
+                <div className='flex flex-row justify-start mt-8'>
                     <TabButton 
                         selectTab={() => handleTabChange("skills")} 
                         active={ tab === "skills"}
@@ -49,6 +93,7 @@ const AboutSection = () => {
                         Certifications{" "} 
                     </TabButton>
                 </div>
+                <div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}</div>
             </div>
         </div>
     </section>
