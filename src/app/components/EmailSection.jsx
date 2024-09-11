cae87c2c80a14ee7f0998f472cbfc6c4217dc8ac
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2024-09-11 13:04:04 -0600

    Tailwind colors from config && message from Email Section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -5,7 +5,8 @@ import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link"
 import Image from "next/image"
 
-const EmailSection = () => {
+const EmailSection = () => {8
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
     const handleSubmit = async (e) =>{
         e.preventDefault();
         const data = {
@@ -38,14 +39,14 @@ const EmailSection = () => {
     
         if (response.status === 200) {
             console.log("Message sent.");
-            //setEmailSubmitted(true);
+            setEmailSubmitted(true);
         }else{
             console.log("Meesage error")
         }
     };  
     return (
         <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
-            <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+            <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
             <div className="z-10">
                 <h5 className="text-xl font-bold text-white my-2">Let&apos;s connect</h5>
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
@@ -103,10 +104,18 @@ const EmailSection = () => {
                     </div>
                     <button 
                         type="submit" 
-                        className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+                        className='bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'
                     >
                         Send Message
-                    </button>   
+                    </button>
+                    {
+                        emailSubmitted && (
+                            <p className="text-green-500 text-sm mt-2">
+                                Email sent successfully!
+                            </p>
+                        )
+
+                    }   
                 </form>
             </div>
         </section>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -23,41 +23,41 @@ const navLinks = [
 ]
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
-  return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
-        <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
-            <Link href="{/}" className="text-2xl md:text-5xl text-white font-semibold">
-                LOGO
-            </Link>
-            <div className='mobile-menu block md:hidden'>
-                {!navbarOpen ? (
-                        <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                            <Bars3Icon className='h-5 w-5'/>
-                        </button>
-                    ):(
-                        <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    )
-                }
+  return(
+        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+            <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
+                <Link href="{/}" className="text-2xl md:text-5xl text-white font-semibold">
+                    LOGO
+                </Link>
+                <div className='mobile-menu block md:hidden'>
+                    {!navbarOpen ? (
+                            <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <Bars3Icon className='h-5 w-5'/>
+                            </button>
+                        ):(
+                            <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        )
+                    }
+                </div>
+                <div className="menu hidden md:block md:w-auto" id='navbar'>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                    {
+                        navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))
+                    }
+                    </ul>
+                </div>
             </div>
-            <div className="menu hidden md:block md:w-auto" id='navbar'>
-                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
-                   {
-                    navLinks.map((link, index) => (
-                        <li key={index}>
-                            <NavLink href={link.path} title={link.title} />
-                        </li>
-                    ))
-                   }
-                </ul>
-            </div>
-        </div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
-  )
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        </nav>
+    )
 }
 
 export default Navbar
